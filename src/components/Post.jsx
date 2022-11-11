3e034b1fa8d86e960e5671c0626f399500cba801
Bruna Domingues Leite <brunadomingues.dev@gmail.com>
2022-11-11 16:14:11 -0300

    Posts com header estilizado, arrumado img, links com as cores certas

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,27 @@
+import styles from './Post.module.css';
 export function Post(){
     return(
-        <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. In possimus ad labore modi harum, fugit alias tempora. Blanditiis voluptates quos, quia animi vitae dolorum odit, minima aliquam eum iste magnam.</h1>
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img src="https://github.com/BrunaDomingues.png/" />
+                    <div className={styles.authorInfo}>
+                        <strong>Bruna Domingues</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+                <time title='11 de maio às 08:13' dateTime='2022-05-11 08:13:30'>Publicado há 1h</time>
+            </header>
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉{' '}<a href="">jane.design/doctorcare</a></p>
+                <p>
+                    <a href="">#novoprojeto</a>{' '}
+                    <a href="">#nlw</a>{' '}
+                    <a href="">#rocketseat</a>
+                </p>
+            </div>
+        </article>
     )
 }
