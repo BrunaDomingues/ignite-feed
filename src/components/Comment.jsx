df79267afd3d2e17c9536dedbb60cd628a1a6107
Bruna Domingues Leite <brunadomingues.dev@gmail.com>
2022-11-28 16:36:11 -0300

    Botão de likes contando clicks

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,9 +1,14 @@
 import styles from './Comment.module.css';
 import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
+import { useState } from 'react';
 export function Comment({content, onDeleteComment}) {
+    const [likeCount, setLikeCount] = useState(0);
     function handleDeleteComment() {
         onDeleteComment(content);
+    }
+    function handleLikeComment(){
+        setLikeCount(likeCount+1);
     }
     return(
         <div className={styles.comment}>
@@ -20,7 +25,7 @@ export function Comment({content, onDeleteComment}) {
                     <p>{content}</p>
                 </div>
                 <footer>
-                    <button><ThumbsUp/> Aplaudir <span>20</span></button>
+                    <button onClick={handleLikeComment}><ThumbsUp/> Aplaudir <span>{likeCount}</span></button>
                 </footer>
             </div>
         </div>
